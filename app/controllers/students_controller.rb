72892ba77fae88c74cb930e51186d98fdd13aa0a
JedwardMook <jedwardmook@yahoo.com>
2022-11-17 13:19:30 -0600

    commit tree 42e4e43b0f6e2d1965ec023fb307f0ab62e7180c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      # students = Student.all.select{|s| s.last_name.downcase == params[:name] || s.first_name.downcase == params[:name]}
+      students = Student.select(:last_name.downcase, :first_name.downcase)
+    else
+      students = Student.all
+    end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
